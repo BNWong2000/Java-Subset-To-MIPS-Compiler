624d01f5fae323765ce1ec3ad10edcead1789196
branden.wong1 <branden.wong1@ms160-1ec.cs.ucalgary.ca>
2022-02-02 19:54:14 -0700

    Completed MS-1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <FlexLexer.h>
 
 #include "main.hpp"
-
+#define MAXNUMWARNINGS 10
 int main(int argc, char* argv[]){
     std::ifstream myFile;
 
@@ -24,20 +24,40 @@ int main(int argc, char* argv[]){
 
     auto lexer = createLexer(myStream);
     int tok;
-
+    int warningCount = 0, warningLine = 0;
     while ((tok = lexer->yylex()) != 0)
     {
-        std::cout << "line: " << lexer->getLine() << " token " << getName(tok);;
+        std::cout << "line: " << lexer->getLine() << " token " << getName(tok);
+        if(tok == T_ERR){
+            return EXIT_FAILURE;
+        }
+
         if(tok == T_STR){
             std::cout << " Lexeme: ";
-            const char *temp = lexer->YYText();
-            int size = sizeof(temp);
+            const char *temp = lexer->YYText();  
+            int size = lexer->YYLeng();
             for(int i = 0; i < size; ++i){
                 std::cout << temp[i];
             }
             std::cout << std::endl;
         }else if (tok == T_ID || tok == T_NUM){
             std::cout << " Lexeme: " << lexer->getLexeme() << "\n";
+        }else if (tok == T_WARN){
+            int lineNum = lexer->getLine();
+            std::cerr << "WARNING: Ignoring an illegal char on line " << lineNum << std::endl;
+            if (warningLine == lineNum)
+            {
+                warningCount++;
+                if(warningCount == MAXNUMWARNINGS){
+                    std::cerr << "ERROR: Too many illegal characters at line " << lineNum << std::endl;
+                    return EXIT_FAILURE;
+                }
+            }
+            else
+            {
+                warningCount = 1;
+                warningLine = lineNum;
+            }
         }else{
             std::cout << "\n";
         }
--- main.hpp
+++ main.hpp
@@ -13,8 +13,6 @@
 enum
 {
     T_ID = 1,
-    T_BEGIN,
-    T_END,
     T_NUM,
     T_ADD,
     T_SUB,
@@ -33,7 +31,24 @@ enum
     T_OR,
     T_IF,
     T_ELSE,
-    T_STR
+    T_WHILE,
+    T_FOR,
+    T_BREAK,
+    T_RET,
+    T_STR,
+    T_LB,
+    T_RB,
+    T_RCB,
+    T_LCB,
+    T_SC,
+    T_COM,
+    T_TRUE,
+    T_FALSE,
+    T_BOOL,
+    T_INT,
+    T_VOID,
+    T_WARN,
+    T_ERR
 };
 
 class myLexer : public yyFlexLexer
@@ -42,6 +57,7 @@ class myLexer : public yyFlexLexer
 public:
     myLexer(std::istream *inputStream) : yyFlexLexer(inputStream) {
         yylineno=1;
+        numWarnings = 0;
     }
 
     virtual ~myLexer() = default;
@@ -53,7 +69,7 @@ public:
     std::string getLexeme(){
         return lexeme;
     }
-
+    int numWarnings;
     std::string lexeme;
 };
 
@@ -63,10 +79,6 @@ inline char const* getName(int token){
     switch(token){
         case T_ID:
             return "ID";
-        case T_BEGIN:
-            return "BEGIN";
-        case T_END:
-            return "END";
         case T_NUM:
             return "NUMBER";
         case T_ADD:
@@ -77,6 +89,8 @@ inline char const* getName(int token){
             return "/";
         case T_MULT:
             return "*";
+        case T_MOD:
+            return "%";
         case T_LT:
             return "<";
         case T_GT:
@@ -98,11 +112,39 @@ inline char const* getName(int token){
         case T_OR:
             return "||";
         case T_IF: 
-            return "IF";
+            return "if";
         case T_ELSE:
-            return "ELSE";
-        case T_STR:
+            return "else";
+        case T_WHILE:
+            return "while";
+        case T_BREAK:
+            return "break";
+        case T_RET:
+            return "return";
+        case T_STR :
             return "STRING";
+        case T_LB:
+            return "(";
+        case T_RB:
+            return ")";
+        case T_LCB:
+            return "{";
+        case T_RCB:
+            return "}";
+        case T_SC:
+            return ";";
+        case T_COM:
+            return ",";
+        case T_TRUE:
+            return "true";
+        case T_FALSE:
+            return "false";
+        case T_BOOL:
+            return "boolean";
+        case T_INT:
+            return "int";
+        case T_VOID:
+            return "void";
         default:
             return "";
     }
