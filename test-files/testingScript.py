88242c71018d905df3eee470d51e4b0cd8f05c58
branden.wong1 <branden.wong1@ms176-2wd.cs.ucalgary.ca>
2022-03-21 11:18:15 -0600

    updated testing script to print more useful things

--- test-files/testingScript.py
+++ test-files/testingScript.py
@@ -8,16 +8,25 @@ outputFile = open("results.txt", "w")
 inputFiles = listdir("SemChecker/")
 inputFiles.sort()
 
+numDiffs = 0
+
 for testFile in inputFiles:
     filePath = "SemChecker/" + testFile
     if(isfile(filePath)):
-        outputFile.write("Testing SemChecker/" + testFile + " - ")
+        # outputFile.write("Testing SemChecker/" + testFile + " - ")
         cp = subprocess.run(["../main", filePath])
-        outputFile.write("Returned " + str(cp.returncode))
+        # outputFile.write("Returned " + str(cp.returncode))
         cpRef = subprocess.run(["/home/profs/aycock/411/TEST/reference/main", "-psemantic", "SemChecker/" + testFile])
-        outputFile.write(", Ref returned " + str(cpRef.returncode) + "\n") 
+        # outputFile.write(", Ref returned " + str(cpRef.returncode) + "\n") 
         if cp.returncode != cpRef.returncode:
             outputFile.write("\n****************** FILE: " + testFile + " FAILED. ******************\n")
+            outputFile.write("Expected a " + str(cpRef.returncode) + ", but recieved a " + str(cp.returncode) + "\n")
+            numDiffs += 1
+
+if numDiffs == 0:
+    outputFile.write("################################## ALL TESTS PASSED. ##################################")
+else:
+    outputFile.write("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX " + str(numDiffs) + " TESTS FAILED. XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 outputFile.close()
 
